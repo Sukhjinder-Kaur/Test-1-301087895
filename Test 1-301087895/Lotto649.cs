ddf5580fd51177e109c80809fc120d81d9c50bf1
Sukhjinder-Kaur <skaur831@my.centennialcollege.ca>
2020-07-15 14:35:17 -0400

    Added the outline given by Professor

--- Test 1-301087895/Lotto649.cs
+++ Test 1-301087895/Lotto649.cs
@@ -5,8 +5,31 @@ using System.Text;
 
 namespace Test_1_301087895
 {
-    public class Lotto649: LottoGame, IGenerateLottoNumbers
+    /**
+     * <summary>
+     * This class is a subclass of the LottoGame abstract superclass
+     * </summary>
+     * 
+     * @class Lotto649
+     */
+    public class Lotto649 : LottoGame, IGenerateLottoNumbers
     {
-    }
+        // CONSTRUCTORS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+        /**
+         * <summary>
+         * This constructor does not take any parameters but satisfies the
+         * base constructor requirements by send the elementNumber and setSize
+         * </summary>
+         * 
+         * @constructor
+         */
+        public Lotto649()
+            : base(6, 49)
+        {
+
+        }
+
+        // CREATE the public GenerateLottoNumbers method here ----------------
+    }
 }
--- Test 1-301087895/LottoMax.cs
+++ Test 1-301087895/LottoMax.cs
@@ -5,7 +5,29 @@ using System.Text;
 
 namespace Test_1_301087895
 {
-    public class LottoMax: LottoGame, IGenerateLottoNumbers
+    /**
+    * <summary>
+    * This class is a subclass of the LottoGame abstract superclass
+    * </summary>
+    * 
+    * @class LottoMax
+    */
+    public class LottoMax : LottoGame, IGenerateLottoNumbers
     {
+        /**
+         * <summary>
+         * This constructor does not take any parameters but satisfies the
+         * base constructor requirements by send the elementNumber and setSize
+         * </summary>
+         * 
+         * @constructor
+         */
+        public LottoMax()
+            : base(7, 49)
+        {
+
+        }
+
+        // CREATE the public GenerateLottoNumbers method here ----------------
     }
 }
